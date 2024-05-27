e8f439df8fd5ccb6247065eab307d4d875661975
ColandSeg <colandseg@gmail.com>
2024-05-27 17:17:55 -0600

    Added data folder and level loading (temp)

--- squareboi/level.py
+++ squareboi/level.py
@@ -1,8 +1,16 @@
-# TODO: write Level class that loads level layouts from files.
-
 class Level:
-    def __init__(self, level_file: str):
-        self.walls = []
+    def __init__(self, level_num: int):
+        self.level_num = level_num
 
     def load_level(self):
-        pass
+        with open(f"data/level_{self.level_num}.txt", "r") as level_file:
+            content = level_file.read()
+            print(content)
+            positions = content.split(";")
+            print(positions)
+            for pos in positions:
+                new_pos = tuple(pos.split(","))
+                print(new_pos, end=" | ")
+
+level_1 = Level(1)
+level_1.load_level()
