684c38457cf92da15a98a8a2bd2a7100834e1c86
ColandSeg <colandseg@gmail.com>
2024-06-03 11:24:02 -0600

    gave match case to Level

--- squareboi/level.py
+++ squareboi/level.py
@@ -13,43 +13,43 @@ class Level:
 
     def __init__(self, lvl_num: int):
         self.lvl_num = lvl_num
+
         self.txt_layout = None
+        self.txt_theme = None
+        self.txt_player = None
+        self.txt_walls = None
 
         with open(f"data/lvl_{self.lvl_num}.txt", "r") as lvl_file:
             self.txt_layout = [line.strip() for line in lvl_file]
-
-    def load_background_path(self):
-        txt_theme = None
-        for line in self.txt_layout:
-            if line[0] == "t":
-                txt_theme = line.split("|")
         
-        theme = THEMES[int(txt_theme[1])]
+        for line in self.txt_layout:
+            match line[0]:
+                case "t": # theme
+                    self.txt_theme = line.split("|")
+                case "p": # player
+                    self.txt_player = line.split("|")
+                case "w": # walls
+                    self.txt_walls = line.split("|")
 
-        return f"{theme}/bg_{theme}"
+    def load_background_path(self) -> str:
+        theme = THEMES[int(self.txt_theme[1])]
+        bg_path = f"{theme}/bg_{theme}"
+
+        return bg_path
     
-    def load_player(self) -> Player:
-        txt_player = None
-        for line in self.txt_layout:
-            if line[0] == "p":
-                txt_player = line.split("|")
-        
-        txt_pos = txt_player[1].split(",")
+    def load_player(self) -> Player:    
+        txt_pos = self.txt_player[1].split(",")
         pos = tuple(int(num) * CELL_LENGTH for num in txt_pos)
-        speed = int(txt_player[2])
+        speed = int(self.txt_player[2])
         player = Player(pos, speed)
 
         return player
     
     def load_walls(self) -> list[Wall]:
-        txt_walls = None
         walls = []
-        for line in self.txt_layout:
-            if line[0] == "w":
-                txt_walls = line.split("|")
         
-        txt_walls.pop(0)
-        for line in txt_walls:
+        self.txt_walls.pop(0)
+        for line in self.txt_walls:
             txt_pos = line.split(",")
             pos = tuple(int(num) * CELL_LENGTH for num in txt_pos)
 
