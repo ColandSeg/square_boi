87351423e2bebae072a58f3ad8ab32c9cf1cb1f3
ColandSeg <colandseg@gmail.com>
2024-08-02 09:41:20 -0600

    The Great Deletion (rewrite)

--- squareboi/objects/wall.py
+++
@@ -1,6 +0,0 @@
-from objects.game_object import GameObject
-from utils import load_img
-
-class Wall(GameObject):
-    def __init__(self, pos: tuple[int, int], theme):
-        super().__init__(load_img(f"{theme}/wall_{theme}.png"), pos)
