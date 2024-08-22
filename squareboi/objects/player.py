8ce8e1d18202f3eb7f402e4602a3ea99e5e1433f
ColandSeg <colandseg@gmail.com>
2024-08-21 19:49:45 -0600

    movement and sprite changing

--- squareboi/objects/player.py
+++ squareboi/objects/player.py
@@ -1,5 +1,6 @@
 from objects.game_object import GameObject
 from utils import load_png
+from pygame.math import Vector2
 from pygame.locals import (
     K_w,
     K_a,
@@ -23,12 +24,37 @@ class Player(GameObject):
         
         super().__init__(self.sprites["front"], pos, speed)
 
-    def update(self, keys: dict):
+    def update(self, keys: dict, screen):
+        self._move(keys)
+        self._change_sprite()
+
+    def _move(self, keys: dict):
+        self.direction *= 0
+
         if keys[K_w]:
-            self.rect.move_ip(0, -4)
-        if keys[K_s]:
-            self.rect.move_ip(0, 4)
+            self.direction.y -= 1
         if keys[K_a]:
-            self.rect.move_ip(-4, 0)
+            self.direction.x -= 1
+        if keys[K_s]:
+            self.direction.y += 1
         if keys[K_d]:
-            self.rect.move_ip(4, 0)
+            self.direction.x += 1
+        
+        self.rect.move_ip(
+            self.direction.x * self.speed,
+            self.direction.y * self.speed
+        )
+    
+    def _change_sprite(self):
+        # Default sprite if `direction` = 0
+        self.surf = self.sprites["front"]
+
+        # Change to straight sprites
+        if self.direction == Vector2(0, -1):
+            self.surf = self.sprites["up"]
+        elif self.direction == Vector2(-1, 0):
+            self.surf = self.sprites["left"]
+        elif self.direction == Vector2(0, 1):
+            self.surf = self.sprites["down"]
+        elif self.direction == Vector2(1, 0):
+            self.surf = self.sprites["right"]
